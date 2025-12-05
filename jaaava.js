0e82b42850401eafbcc91e4c73f2275ea0cb99d7
Aradhya89 <aradhyagupta637@gmail.com>
2025-12-05 10:45:38 +0530

    modifying website

---
+++ jaaava.js
@@ -0,0 +1,10 @@
+// const items = document.querySelectorAll('.fade');
+
+// window.addEventListener('scroll', () => {
+//     items.forEach(item => {
+//         const pos = item.getBoundingClientRect().top;
+//         if(pos < window.innerHeight - 100) {
+//             item.classList.add('show');
+//         }
+//     });
+// });
